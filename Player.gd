7605b341cd2b67245127ab44e90324dae5cfdcac
Jonathan Carey <jonathan.carey1995@gmail.com>
2024-08-30 14:25:12 -0400

    added custom inputs, refactored code, added connecting levels

--- Player.gd
+++ Player.gd
@@ -1,13 +1,33 @@
 extends RigidBody3D
 
+## How much vertical force to apply when moving.
+@export_range(750.0, 3000.0) var thrust: float = 1000.0
+
+## How much torque force to apply to the ship.
+@export_range(50.0, 300.0) var torque_thrust: float = 100.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_accept"):
-		apply_central_force(basis.y * delta * 1000.0)
+	if Input.is_action_pressed("boost"):
+		apply_central_force(basis.y * delta * thrust)
 		
-	if Input.is_action_pressed("ui_left"):
-		apply_torque(Vector3(0.0, 0.0, 100.0 * delta))
+	if Input.is_action_pressed("rotate_left"):
+		apply_torque(Vector3(0.0, 0.0, torque_thrust * delta))
 		
-	if Input.is_action_pressed("ui_right"):
-		apply_torque(Vector3(0.0, 0.0, -100.0 * delta))
+	if Input.is_action_pressed("rotate_right"):
+		apply_torque(Vector3(0.0, 0.0, -torque_thrust * delta))
+
+
+func _on_body_entered(body: Node) -> void:
+	if "Goal" in body.get_groups():
+		complete_level(body.file_path)     
+	if "Crash" in body.get_groups():
+		crash_sequence()
+
+func crash_sequence() -> void:
+	print("KABOOM!")
+	get_tree().reload_current_scene()
+
+func complete_level(next_level_file: String) -> void:
+	print("You win!")
+	get_tree().change_scene_to_file(next_level_file)
